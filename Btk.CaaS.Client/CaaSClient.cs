e6714c63e99d823b9fc0465d183072fe93e76f01
Tomasz Batko <dekster.z@gmail.com>
2014-11-21 00:24:28 +0100

    Some chages in Index, added WCF Client Project

---
+++ Btk.CaaS.Client/CaaSClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Btk.CaaS.Client
+{
+    public class CaaSClient : ClientBase<ICaaSService>, ICaaSService
+    {
+        public string Cowsay(string message)
+        {
+            return base.Channel.Cowsay(message);
+        }
+    }
+}
---
+++ Btk.CaaS.Client/ICaaSService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Btk.CaaS.Client
+{
+    [ServiceContract]
+    public interface ICaaSService
+    {
+        [WebInvoke(
+            Method = "POST",
+            BodyStyle = WebMessageBodyStyle.Bare,
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            UriTemplate = "uri/cowsay")]
+        string Cowsay(string message);
+    }
+}
