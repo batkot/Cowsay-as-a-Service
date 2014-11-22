78ed56d555b340b396b0b5e22c82164dbae9366c
Tomasz Batko <dekster.z@gmail.com>
2014-11-22 01:14:28 +0100

    CowSay refresh on main page

--- Btk.CaaS/App_Start/BundleConfig.cs
+++ Btk.CaaS/App_Start/BundleConfig.cs
@@ -16,6 +16,9 @@ namespace Btk.CaaS
                         "~/Scripts/TimeSpan-{version}.js").Include(
                         "~/Scripts/countdown.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/cow").Include(
+                        "~/Scripts/cowAnim.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
---
+++ Btk.CaaS/Scripts/cowAnim.js
@@ -0,0 +1,28 @@
+﻿
+setInterval(
+    function () {
+        getCow(function (cow) {
+            display(cow)
+        }
+        )
+    }, 10000);
+
+function getCow(display)
+{
+    $.ajax(
+        {
+            type: "POST",
+            url: "api/cowsay",
+            contentType: "application/json",
+            data: "",
+            success: function(cow) 
+            {
+                display(cow);
+            }
+        })
+}
+
+function display(cow)
+{
+    $("#cow").html(cow);
+}
